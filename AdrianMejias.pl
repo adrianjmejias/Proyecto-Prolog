fda89f717c5653d14e0e970f969af03be0166985
adrianjmejias <adrianjmejias@gmail.com>
2018-05-24 00:04:18 -0400

    3 terminado

--- AdrianMejias.pl
+++ AdrianMejias.pl
@@ -2,38 +2,79 @@
 
 % n_periodo()
 
-caracter_valido(X):- X >= "A", X =< "Z", !.
-caracter_valido(X):- X >= "a", X =< "z", !.
-caracter_valido('!'):-!.
+check_valid_char_list([]).
+check_valid_char_list([X | Xs]):-
+    caracter_valido(X), check_valid_char_list(Xs)
+.
+
+
+
+caracter_valido(X):- char_type(X, alpha), !.
 caracter_valido(','):-!.
-caracter_valido(':'):-!.
+caracter_valido('!'):-!.
+caracter_valido('.'):-!.
 caracter_valido(':'):-!.
 caracter_valido(';'):-!.
 caracter_valido('?'):-!.
 caracter_valido(' '):-!.
 
-reversed_num_list([],[]).
-reversed_num_list([E | Es], R):- 
-    caracter_valido(E)
-    , number_chars(E, List)
+char_list_to_code_list([], []).
+char_list_to_code_list([C | Chars], Code):-
+    char_list_to_code_list(Chars, NextCode)
+    , char_code(C, CodedChar)
+    , Code = [CodedChar | NextCode]
+.
+
+code_list_to_char_list([], []).
+code_list_to_char_list([Code | Codes], Chars):-
+    atom_number(Code, Num)
+    , char_code(Char, Num)
+    , code_list_to_char_list(Codes, NextChars)
+    , Chars = [Char | NextChars]
+.
+
+reversed_atom_list([],[]).
+reversed_atom_list([E | Es], R):- 
+     number_chars(E, List)
     , reverse(List, Reversed)
     , atomic_list_concat(Reversed, Atom)
-    , reversed_num_list(Es, Rnext)
-    , atom_number(Atom, Num)
-    , R = [Num | Rnext]
-    .
+    , reversed_atom_list(Es, Rnext)
+    , R = [Atom | Rnext]
+.
+
+char_list_to_atoms([], []).
+char_list_to_atoms(['1' | [D1 | [D2 | Rest]]], AtomList):-
+    atomic_list_concat([1, D1, D2], Atom)
+    , char_list_to_atoms(Rest, NAtoms)
+    , AtomList = [Atom | NAtoms]
+    , !
+.
+char_list_to_atoms([D1 | [D2 | Rest]], AtomList):-
+    atomic_list_concat([D1, D2], Atom)
+    , char_list_to_atoms(Rest, NAtoms)
+    , AtomList = [Atom | NAtoms]
+    , !
+.
 
 %cifrar
 transformar(1, Input, Out):- 
-    atom_codes(Input, Atoms)
-    , reverse(Atoms, Reversed)
-    %Aca tengo que hacer validacion y reverse de los caracteres en la lista
-    , reversed_num_list(Reversed, AsciiReversed)
-    , atomic_list_concat(AsciiReversed, '', Aux)
-    , atom_string(Aux, Out)
-    .
-%cifrar
-transformar(0, S, M).
+    string_chars(Input, Chars)
+    , check_valid_char_list(Chars)
+    , char_list_to_code_list(Chars, Codes)
+    , reverse(Codes, Reversed)
+    , reversed_atom_list(Reversed, ReversedCodes)
+    , atomics_to_string(ReversedCodes, Out) 
+.
+
+%decifrar
+transformar(0, Input, Out):-
+    string_chars(Input, Chars)
+    , reverse(Chars, Reversed)
+    , char_list_to_atoms(Reversed, AtomList)
+    , code_list_to_char_list(AtomList, CharList)
+    , atomics_to_string(CharList, Out)
+.
+
 % 2
 
 % 3
